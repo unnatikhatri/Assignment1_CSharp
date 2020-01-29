843f372ff59140ed4baf31f9b772fb39233ec493
unnatikhatri <59996056+unnatikhatri@users.noreply.github.com>
2020-01-29 15:15:52 +0530

    Add files via upload

---
+++ PromactInfo/Employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromactInfo
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private string deptName;
+
+        public Employee(int id, string name, string dept)
+        {
+            this.id = Id;
+            this.name = Name;
+            this.deptName = DeptName;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string DeptName
+        {
+            get
+            {
+                return deptName;
+            }
+            set
+            {
+                deptName = value;
+            }
+        }
+
+
+        public int GetId()
+        {
+            return Id;
+        }
+        public string GetName()
+        { 
+            return Name;
+        }
+        public string GetDeptName()
+        {
+            return DeptName;
+        }
+        public string GetDeptName(string dept)
+        {
+            return dept;
+        }
+
+        public string GetDeptName(string existingDept, string newDept)
+        {
+            return newDept;
+        }
+
+        public string GetDeptName(string dept, int id)
+        {
+            return dept; ;
+        }
+    }
+}
---
+++ PromactInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromactInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Id :");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Name :");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Department Name :");
+            string deptName = Console.ReadLine();
+
+            Employee emp = new Employee(id, name, deptName);
+
+            Console.WriteLine("Employee ID : {0}", emp.GetId());
+            Console.WriteLine("Employee Name : {0}", emp.GetName());
+            Console.WriteLine("Employee Department Name : {0}", emp.GetDeptName());
+
+        }
+
+    }
+}
